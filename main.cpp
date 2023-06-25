1ac4b216c25cd6eeaccdc0e816b9399cd8d9a3a5
Michal Zalobny <michal.zalobny@gmail.com>
2023-06-25 23:53:20 +0200

    Add vertex buffer object and store it on gpu so we can later use it in the vertex shader

--- main.cpp
+++ main.cpp
@@ -44,6 +44,18 @@ int main()
         return -1;
     }    
 
+    // build and compile our shader program
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f
+    };
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // 30 page
+ 
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
