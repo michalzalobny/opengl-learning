63f0fb28ae63753a609f1856eeb384b856638db2
Michal Zalobny <michal.zalobny@gmail.com>
2023-06-27 14:35:19 +0200

    Animate uniforms

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <cmath>
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -13,21 +14,26 @@ const unsigned int SCR_HEIGHT = 600;
 
 const char *vertexShaderSource = "#version 330 core\n"
                                  "layout (location = 0) in vec3 aPos;\n"
+                                 "out vec4 vertexColor;\n"
                                  "void main()\n"
                                  "{\n"
                                  "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                 "   vertexColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n"
                                  "}\0";
 const char *fragmentShader1Source = "#version 330 core\n"
+                                    "in vec4 vertexColor;\n"
                                     "out vec4 FragColor;\n"
+                                    "uniform vec4 ourColor;\n"
                                     "void main()\n"
                                     "{\n"
-                                    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                    "   FragColor = ourColor;\n"
                                     "}\n\0";
 const char *fragmentShader2Source = "#version 330 core\n"
                                     "out vec4 FragColor;\n"
+                                    "uniform vec4 ourColor;\n"
                                     "void main()\n"
                                     "{\n"
-                                    "   FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
+                                    "   FragColor = ourColor;\n"
                                     "}\n\0";
 
 int main()
@@ -135,8 +141,14 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        float timeValue = glfwGetTime();
+        float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+        int vertexColorLocation = glGetUniformLocation(shaderProgramYellow, "ourColor");
+        glUseProgram(shaderProgramYellow);
+        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
         // now when we draw the triangle we first use the vertex and orange fragment shader from the first program
-        glUseProgram(shaderProgramOrange);
+        // glUseProgram(shaderProgramOrange);
         // draw the first triangle using the data from our first VAO
         glBindVertexArray(VAOs[0]);
         glDrawArrays(GL_TRIANGLES, 0, 3); // this call should output an orange triangle
